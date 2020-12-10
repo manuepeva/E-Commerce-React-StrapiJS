93c17f804628e6e9ae294f7268471073b6576ccb
ManuelP <manuepeva@gmail.com>
2020-12-10 09:59:15 -0500

    Solving issues with fetching products method of commercejs

---
+++ e-commerce-new/src/components/cart/Cart.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import {Container, Typography, Button, Grid} from '@material-ui/core'
+import useStyles from './stylesCart'
+import CartItem from './cartItem/CartItem'
+import {Link} from 'react-router-dom'
+
+const Cart = ({cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart}) => {
+    const classes = useStyles()
+    const EmptyCard = () => (
+        <Typography variant="subtitle1">
+            You Have No Items in Your Shopping.
+                <Link to="/" className={classes.link}>
+                Start Adding Something!
+                </Link>!
+            </Typography>
+    )
+    const FilledCart = () => (
+        <div>
+            <Grid container spacing={3}>
+                {cart.line_items.map((item) => (
+                    <Grid item xs={12} sm={4} key={item.id}>
+                        {/* <CartItem /> */}
+                        <CartItem item={item} 
+                        handleUpdateCartQty={handleUpdateCartQty}
+                        handleRemoveFromCart={handleRemoveFromCart}
+                        />
+                    </Grid>
+                ))}
+            </Grid>
+            <div className={classes.cardDetails}>
+                <Typography variant="h4">
+                    Subtotal: {cart.subtotal.formatted_with_symbol}
+                </Typography>
+                <div>
+                    <Button className={classes.emptyButton}
+                    size="large" 
+                    type="button"
+                    variant="contained"
+                    color="secondary"
+                    onClick={handleEmptyCart}
+                    >Empty Cart
+                    </Button>
+                    <Button className={classes.checkoutButton}
+                    size="large" 
+                    type="button"
+                    variant="contained"
+                    color="primary"
+                    component={Link}
+                    to="/checkout"
+                    >Checkout
+                    </Button>
+                </div>
+            </div>
+        </div>
+    )
+
+    if(!cart.line_items) return 'Loading...'
+
+    return (
+        <Container>
+            <div className={classes.toolbar} />
+            <Typography className={classes.title} 
+            variant="h3" gutterBottom>Your Shopping Cart</Typography>
+            {!cart.line_items.length ? <EmptyCard /> : <FilledCart />}
+        </Container>
+    )
+}
+
+export default Cart
---
+++ e-commerce-new/src/components/products/Products.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import {Grid} from '@material-ui/core'
+import Product from '../product/Product'
+import img1 from '../../img/e-commerce-shoe-1.png'
+import img2 from '../../img/e-commerce-shoe-2.png'
+import useStyles from './stylesProducts'
+
+
+const Products = ({products, handleAddToCart}) => {
+    const classes = useStyles()
+    return (
+        <main className={classes.content}>
+            <div className={classes.toolbar}/>
+            <Grid
+            contained
+            justify="center"
+            spacing={4}
+            >
+                {
+                    products.map((product) =>(
+                        <Grid item key={product.id} 
+                        xs={12} sm={6} md={4} lg={3}
+                        >
+                        <Product product={product} handleAddToCart={handleAddToCart}/>
+                        </Grid>
+                    ))
+                }
+            </Grid>
+        </main>
+    )
+}
+
+export default Products
